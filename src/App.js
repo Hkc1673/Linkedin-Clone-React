504470f0ea0bde25ab63c2754f9573eb58d8caf9
Hkc-1673 <habibkc71@gmail.com>
2020-12-27 03:15:51 +0200

    Added login page, firebase and modified redux files

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import Header from "./Header"
 import Sidebar from "./Sidebar"
 import Feed from "./Feed"
 import './App.css';
+import { useSelector } from "react-redux";
+import { selectUser } from './features/userSlice';
+import Login from "./Login"
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
-     <Header/>
-     <div className="app__body">
-       <Sidebar/>
-       <Feed/>
-     </div>
+      <Header />
+      {
+        !user ? (
+          <Login />
+        ) : (
+            <div className="app__body">
+              <Sidebar />
+              <Feed />
+            </div>
+          )}
     </div>
   );
 }
